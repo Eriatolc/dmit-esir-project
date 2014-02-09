d0827e85178c7ff22e78580606edba2a02cec73c
Clotaire <clo.delanchy@gmail.com>
2014-02-09 13:44:50 +0100

    Clean the www/ code and directories

--- www/portal/js/main.js
+++ www/portal/js/main.js
@@ -7,7 +7,7 @@
 function init() {
 
 	// Set the listeners on the webpage
-	setListeners();
+	//setListeners();
 
 	// Get the Alert with an XHR
 	var xhr = new XMLHttpRequest();
@@ -25,6 +25,10 @@ function init() {
 	document.getElementById('alertTab').innerHTML += html;
 }
 
+/** Prepare the data before being displayed
+ * @param {Object} response - XHR HTTP response
+ * @return data - cleaned data
+ */
 function prepareData(response) {
 	var data = response;
 	var item;
@@ -39,15 +43,15 @@ function prepareData(response) {
 	return data;
 }
 
-function setListeners() {
-	var list = document.getElementById('graph');
-	list.addEventListener('change', function() {
-		// We get the content when the selectedIndex change and we display it
-		// in the webpage correctly (above the graph)
-		var value = list.options[list.selectedIndex].innerHTML;
-		document.getElementById('graphTitle').innerHTML = value;
-	}, true);
-}
+// function setListeners() {
+// 	var list = document.getElementById('graph');
+// 	list.addEventListener('change', function() {
+// 		// We get the content when the selectedIndex change and we display it
+// 		// in the webpage correctly (above the graph)
+// 		var value = list.options[list.selectedIndex].innerHTML;
+// 		document.getElementById('graphTitle').innerHTML = value;
+// 	}, true);
+// }
 
 // We use the init function only when the DOM is loaded
 document.addEventListener('DOMContentLoaded', init, false);
@@ -70,87 +74,4 @@ var data = {
 			data : [28,48,40,19,96,27,100]
 		}
 	]
-}
-
-
-//Get the context of the canvas element we want to select
-var ctx = document.getElementById("myChart").getContext("2d");
-var Bar = new Chart(ctx).PolarArea(data);
-
-//creation
-new Chart(ctx).Bar(data);
-
-Bar.defaults = {
-				
-	//Boolean - If we show the scale above the chart data			
-	scaleOverlay : false,
-	
-	//Boolean - If we want to override with a hard coded scale
-	scaleOverride : false,
-	
-	//** Required if scaleOverride is true **
-	//Number - The number of steps in a hard coded scale
-	scaleSteps : null,
-	//Number - The value jump in the hard coded scale
-	scaleStepWidth : null,
-	//Number - The scale starting value
-	scaleStartValue : null,
-
-	//String - Colour of the scale line	
-	scaleLineColor : "rgba(0,0,0,.1)",
-	
-	//Number - Pixel width of the scale line	
-	scaleLineWidth : 1,
-
-	//Boolean - Whether to show labels on the scale	
-	scaleShowLabels : true,
-	
-	//Interpolated JS string - can access value
-	scaleLabel : "<%=value%>",
-	
-	//String - Scale label font declaration for the scale label
-	scaleFontFamily : "'Arial'",
-	
-	//Number - Scale label font size in pixels	
-	scaleFontSize : 12,
-	
-	//String - Scale label font weight style	
-	scaleFontStyle : "normal",
-	
-	//String - Scale label font colour	
-	scaleFontColor : "#666",	
-	
-	///Boolean - Whether grid lines are shown across the chart
-	scaleShowGridLines : true,
-	
-	//String - Colour of the grid lines
-	scaleGridLineColor : "rgba(0,0,0,.05)",
-	
-	//Number - Width of the grid lines
-	scaleGridLineWidth : 1,	
-
-	//Boolean - If there is a stroke on each bar	
-	barShowStroke : true,
-	
-	//Number - Pixel width of the bar stroke	
-	barStrokeWidth : 2,
-	
-	//Number - Spacing between each of the X value sets
-	barValueSpacing : 5,
-	
-	//Number - Spacing between data sets within X values
-	barDatasetSpacing : 1,
-	
-	//Boolean - Whether to animate the chart
-	animation : false,
-
-	//Number - Number of animation steps
-	animationSteps : 60,
-	
-	//String - Animation easing effect
-	animationEasing : "easeOutQuart",
-
-	//Function - Fires when the animation is complete
-	onAnimationComplete : null
-	
 }
